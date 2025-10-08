d30c1924c92a4e0abf9e57181b59faa19c71c81f
Magali Dauvin <67729174+madauvin@users.noreply.github.com>
2025-10-08 12:14:33 +0200

    mise à jour

--- simplemodeleEPU.R
+++ simplemodeleEPU.R
@@ -1,10 +1,11 @@
 library(tidyverse)
 library(zoo)
 library(insee)
-library(timeSeries)
+library(tseries)
+library(readxl)
+library(dplyr)
 
-
-data_PIB <- read_excel("Desktop/data_PIB.xlsx")
+data_PIB <- read_excel("data_PIB.xlsx")
 vintage <-"oui"
 
 # Création d’une variable de trimestre
@@ -39,9 +40,6 @@ df_quarterly <- df_quarterly |>
     quarter = as.numeric(unlist(quarter))
   )
 
-##### J'enlève ce qui est après 2025Q2
-df_travail <- df_quarterly[-c(123:127),]
-
 
 if(vintage=="non"){
 dataset <- get_dataset_list()
@@ -52,47 +50,73 @@ PIB_vol <- get_insee_idbank("011794844") |>
   dplyr::rename("date"=TIME_PERIOD, 
                 "PIB"=OBS_VALUE)
 }else{
-  PIB_vol <- read_csv("Desktop/PIB_millesime.csv")
-  PIB_vol <- PIB_vol |>
-  mutate(date = paste0(year(DATE), "-Q", quarter(DATE)))
+  PIB_vol <- read_csv("PIB_millesime.csv", show_col_types = FALSE) |> 
+    dplyr::rename(date=DATE)
 }
 
+df_quarterly <- df_quarterly |> 
+  mutate(date=yq(paste0(year, ":Q", quarter))) |> 
+  left_join(PIB_vol, by = "date") |> 
+  mutate(lagEPU = dplyr::lag(EPU,1), 
+         dEPU = EPU - dplyr::lag(EPU,1))
 
-df_travail_final <- df_travail |> 
-  left_join(PIB_vol, by = "date")
 
-## Je fais à partir de 2010 (par contre je laisse jusqu'à 2025 pour avoir suffisemment d'observations.)
-#' Je mettrais des dummies après observations des résiuds
-df_travail_final <- df_travail_final[-c(1:60),]
+plot(df_travail$dEPU, type="l")
+df_travail <- df_quarterly |> 
+na.omit()
 
-modele_ols <- lm(PIB ~ F1 + F2 + F3 , data = df_travail_final)
+df_travail2010 <- df_travail |> 
+  dplyr::filter(date>="2009-07-01") |> 
+  mutate(date=as.Date(date))
 
-# Résumé complet
+adf.test(df_travail2010$EPU)
+adf.test(df_travail2010$dEPU)
+
+
+moy <- mean(df_travail2010$EPU, na.rm = TRUE)
+ecart <- sd(df_travail2010$EPU, na.rm = TRUE)
+moydEPU <- mean(df_travail2010$dEPU, na.rm = TRUE)
+ecartdEPU <- sd(df_travail2010$dEPU, na.rm = TRUE)
+# Création de la dummy
+df_travail2010$dummy_EPU <- ifelse(df_travail2010$EPU > moy +  ecart, df_travail2010$EPU, 0)
+df_travail2010$dummy_dEPU <- ifelse(df_travail2010$dEPU > moydEPU +  ecartdEPU, df_travail2010$EPU, 0)
+#' le problème de cette variable est qu'elle nous indique une non stationnarité
+#' 
+df_travail2010sans2020 <- df_travail2010 %>%
+  mutate(date = as.Date(date)) %>%
+  dplyr::filter(
+    !(year(date) == 2020 | (year(date) == 2021 & quarter(date) %in% c(1, 2, 3)))
+  )
+
+
+#' Modèle OLS, 3 facteurs 3 dummies
+modele_ols <- lm(PIB ~ F1 + F2 + F3 , data = df_travail2010sans2020)
 summary(modele_ols)
-residus <- resid(modele_ols)
+df_travail2010sans2020$res_modele_ols <- resid(modele_ols)
+
 
 plot(resid(modele_ols), type = "l",
      main = "Résidus du modèle OLS", xlab = "Date", ylab = "Résidus")
 
-## Ajout des dummmies
-df_travail_final <- df_travail_final |>
-  mutate(
-    D102 = ifelse(row_number() == 41, 1, 0),
-    D103 = ifelse(row_number() == 42, 1, 0),
-    D104 = ifelse(row_number() == 43, 1, 0),
-  )
+prevT4 <- coef(modele_ols)[1]*1+coef(modele_ols)[2]*df_quarterly$F1[which(df_quarterly$date=="2025-10-01")]+coef(modele_ols)[3]*df_quarterly$F2[which(df_quarterly$date=="2025-10-01")]+coef(modele_ols)[4]*df_quarterly$F3[which(df_quarterly$date=="2025-10-01")]
 
-#' Modèle OLS, 3 facteurs 3 dummies
-modele_ols <- lm(PIB ~ F1 + F2 + F3+ D102 + D103  + D104, data = df_travail_final)
+#' Modèle OLS, 3 facteurs EPU
+modele_ols_EPU <- lm(PIB ~ F1 + F2 + F3 + EPU, data = df_travail2010sans2020)
+summary(modele_ols_EPU)
 
-summary(modele_ols)
-prevT3 <- coef(modele_ols)[1]*1+coef(modele_ols)[2]*df_quarterly$F1[which(df_quarterly$date=="2025-Q3")]+coef(modele_ols)[3]*df_quarterly$F2[which(df_quarterly$date=="2025-Q3")]
+prevT4_EPU <- coef(modele_ols_EPU)[1]*1+coef(modele_ols_EPU)[2]*df_quarterly$F1[which(df_quarterly$date=="2025-10-01")]+coef(modele_ols_EPU)[3]*df_quarterly$F2[which(df_quarterly$date=="2025-10-01")]+coef(modele_ols_EPU)[4]*df_quarterly$F3[which(df_quarterly$date=="2025-10-01")]+coef(modele_ols_EPU)[5]*df_quarterly$EPU[which(df_quarterly$date=="2025-10-01")]
 
-#' Même modèle avec ajout de l'EPU.
-modele_ols_avecEPU <- lm(PIB ~ F1 + F2 +F3 + EPU+ D102 + D103  + D104, data = df_travail_final)
-summary(modele_ols_avecEPU)
+#' Même modèle avec ajout du lagEPU.
+modele_ols_lagEPU <- lm(PIB ~ F1 + F2 +F3 + EPU + lagEPU, data = df_travail2010sans2020)
+summary(modele_ols_lagEPU)
 
-prevT3_avecEPU <- coef(modele_ols_avecEPU)[1]*1+coef(modele_ols_avecEPU )[2]*df_quarterly$F1[which(df_quarterly$date=="2025-Q3")]+coef(modele_ols_avecEPU)[3]*df_quarterly$F2[which(df_quarterly$date=="2025-Q3")]+coef(modele_ols_avecEPU)[4]*df_quarterly$F3[which(df_quarterly$date=="2025-Q3")]+coef(modele_ols_avecEPU)[5]*df_quarterly$EPU[which(df_quarterly$date=="2025-Q3")]
+prevT4_lagEPU <- coef(modele_ols_EPU)[1]*1+coef(modele_ols_lagEPU)[2]*df_quarterly$F1[which(df_quarterly$date=="2025-10-01")]+coef(modele_ols_lagEPU)[3]*df_quarterly$F2[which(df_quarterly$date=="2025-10-01")]+coef(modele_ols_lagEPU)[4]*df_quarterly$F3[which(df_quarterly$date=="2025-10-01")]+coef(modele_ols_lagEPU)[5]*df_quarterly$EPU[which(df_quarterly$date=="2025-10-01")]+coef(modele_ols_lagEPU)[6]*df_quarterly$lagEPU[which(df_quarterly$date=="2025-10-01")]
 
-cat(paste0("Le modèle avec les 3 facteurs indiquent une croissance du PIB au T4 2025 de ", round(prevT3, 2), ", tandis quel l'ajout de l'EPU dans l'équation et la simulation du modèle avec en hypothèse un niveau maintenu au dernier connu ampute la prévision de la croissance du PIB du T4 2025 de ", round(prevT3 -prevT3_avecEPU, 2), " point."))
 
+
+modele_ols_dum1EPU <- lm(PIB ~ F1 + F2 +F3 + dummy_EPU, data = df_travail2010sans2020)
+summary(modele_ols_dum1EPU)
+
+
+modele_ols_dum2EPU <- lm(PIB ~ F1 + F2 +F3 + dummy_dEPU, data = df_travail2010sans2020)
+summary(modele_ols_dum2EPU)
